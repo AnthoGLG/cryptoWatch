261024834dc7eef0e730620340373458949c72d0
Anthony Gallego <3igweb@gmail.com>
2021-01-21 11:21:13 +0100

    Realisation du Layout

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import Layout from "../components/Layout";
+
+export default function About() {
+  return (
+    <Layout page="À propos">
+      <h1 className="text-4xl">About</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente animi
+        exercitationem ex inventore quis asperiores consequuntur deserunt a
+        dolorum, ratione consectetur fugit debitis quidem, adipisci voluptates
+        distinctio pariatur officiis odio quo reiciendis quod eius illum minus
+        minima. Exercitationem beatae et at suscipit non reprehenderit quae sit
+        illum veniam, vero doloribus consectetur quia soluta voluptates unde
+        eaque id ad. Quo atque ratione debitis magni, omnis soluta molestias
+        dolor sint unde est odit, assumenda provident? Dignissimos alias
+        architecto mollitia consectetur tenetur iusto.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        voluptatibus, rerum omnis nam nobis praesentium, vitae, doloremque
+        pariatur consequuntur blanditiis reprehenderit necessitatibus earum qui
+        ad aliquid dolorum voluptate ullam dicta eligendi. Unde placeat omnis
+        sapiente ex nulla. Quia voluptatem adipisci in possimus recusandae modi
+        asperiores sit voluptatibus commodi, amet aliquam quod. Praesentium
+        voluptatem rem, quia doloremque, ad dolores ab debitis dolorem quasi
+        expedita et eligendi cumque, quidem tempore consectetur recusandae.
+      </p>
+    </Layout>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
-  return <div>Hello Next</div>;
+  return (
+    <Layout page="Crypto Watch - Accueil">
+      <h1>ACCUEIL</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum maxime
+        quod incidunt facere voluptate tempore temporibus sit tempora amet id?
+        Inventore ratione perspiciatis, quos ab officiis facere nisi itaque id
+        voluptates incidunt quam magnam aliquid tenetur dolorum odio maxime,
+        suscipit adipisci nulla. Voluptatum tempora est soluta vel ad totam.
+        Quibusdam, quam? Distinctio, ad laboriosam! Tenetur error accusantium
+        excepturi quam. Mollitia hic nobis, dignissimos maxime qui eveniet
+        possimus veritatis, quisquam saepe facilis natus aspernatur nihil
+        praesentium excepturi, iusto sint fuga illum.
+      </p>
+    </Layout>
+  );
 }
